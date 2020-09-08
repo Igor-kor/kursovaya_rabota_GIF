03930b564aa85dbc9c1f09957c7c0ed9c0ed2642
Igor Sharangiya <igor_korenovsk@mail.ru>
2020-09-08 14:54:20 +0300

    init

---
+++ kursovaya_rabota_3_semestr/Gif.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursovaya_rabota_3_semestr
+{
+    class Gif
+    {
+        // Заголовок
+        string title;
+        // Логический дескриптор экрана
+        Descriptor descriptor;
+        //Глобальная таблица цветов
+        GlobalColorTable globalColorTable;
+        //Блок расширения
+        Extension extension;
+        //Блок изображения
+        Picture[] picture;
+        // Блок завершения файла
+        EndFile endFile;
+        // Комментарий
+        String comment;
+
+
+        public Gif()
+        {
+            title = "GIF89a";
+            comment = "Курсовая работа, Шарангия Игорь, igor_korenovsk@mail.ru";
+            descriptor = new Descriptor();
+        }
+
+        public string Generate()
+        {
+            return descriptor.ToString();
+        }
+    }
+
+    // логический дескриптор экрана
+    class Descriptor
+    {
+        string W;
+        string H;
+        string BG;
+        string R;
+        string CT;
+        string Size;
+        string Color;
+        string SF;
+        public Descriptor()
+        {       
+            W = "100";
+            H = "100";
+            BG = "0";
+            R = "00";
+            CT = "0";
+            Size = "000";
+            Color = "7";
+            SF = "0";
+        }
+    }
+
+    class GlobalColorTable
+    {
+
+    }
+
+    class Extension
+    {
+
+    }
+
+    class Picture
+    {
+
+    }
+
+    class EndFile
+    {
+
+    }
+}
